d2aa2286e836913335617ed8b77a462debeae2c6
Muhammadaziz <salohiddinov.mm@gmail.com>
2024-06-14 23:20:48 +0500

    router file edited

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../pages/HomePage/HomePage.vue'
-import LoginPage from '../pages/LoginPage/LoginPage.vue'
-import ReportersPage from '../pages/ReportersPage/ReportersPage.vue'
-import PatientsPage from '../pages/PatientsPage/PatientsPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage/HomePage.vue";
+import LoginPage from "../pages/LoginPage/LoginPage.vue";
+import ReportersPage from "../pages/ReportersPage/ReportersPage.vue";
+import PatientsPage from "../pages/PatientsPage/PatientsPage.vue";
+import PatientPage from "../pages/PatientsPage/PatientPage/PatientPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomePage
+      path: "/",
+      name: "home",
+      component: HomePage,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginPage
+      path: "/login",
+      name: "login",
+      component: LoginPage,
     },
     {
-      path: '/reporters',
-      name: 'reporters',
-      component: ReportersPage
+      path: "/reporters",
+      name: "reporters",
+      component: ReportersPage,
     },
     {
-      path: '/patients',
-      name: 'patients',
-      component: PatientsPage
+      path: "/patients",
+      name: "patients",
+      component: PatientsPage,
+      // children: [
+        
+      // ],
     },
-  ]
-})
+    {
+      path: "/patients/patient/:id", // Dynamic route for patient page with id parameter
+      name: "patient", // Name for the route
+      component: PatientPage,
+      props: true,
+    },
+  ],
+});
 
-export default router
+export default router;
