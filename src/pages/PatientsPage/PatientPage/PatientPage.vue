0ec66c2b409ee96fd380fee27fd8960d5b4d7289
Muhammadaziz <salohiddinov.mm@gmail.com>
2024-06-18 19:36:53 +0500

    some funcs added/edited

--- src/pages/PatientsPage/PatientPage/PatientPage.vue
+++ src/pages/PatientsPage/PatientPage/PatientPage.vue
@@ -63,9 +63,9 @@ const format = (date) => {
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
 
-  return `${day > 9 ? day : '0' + day}/${
+  return `${day > 9 ? day : '0' + day}.${
     month > 9 ? month : '0' + month
-  }/${year}`;
+  }.${year}`;
 };
 
 function submit() {
